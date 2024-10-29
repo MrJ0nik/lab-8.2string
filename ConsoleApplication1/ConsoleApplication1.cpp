f930994eba91044ab4aff3f2b2017c47c00eeb88
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-10-29 19:17:29 +0200

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void capitalizeAfterDot(string& s) {
+    bool capitalize = false;
+
+    for (size_t i = 0; i < s.length(); i++) {
+        if (s[i] == '.') {
+            capitalize = true;
+        }
+        else if (capitalize && isalpha(s[i])) {
+            s[i] = toupper(s[i]);
+            capitalize = false;
+        }
+        else if (isspace(s[i])) {
+            continue;
+        }
+        else {
+            capitalize = false;
+        }
+    }
+}
+
+int main() {
+    string str;
+
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+
+    capitalizeAfterDot(str);
+
+    cout << "Transformed string: " << str << endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication1/ConsoleApplication1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			string test1 = ".Leading dot and trailing dot.Hello. This is a test.";
+			capitalizeAfterDot(test1);
+			Assert::AreEqual(string(".Leading dot and trailing dot.Hello. This is a test."), test1);
+
+		}
+	};
+}
